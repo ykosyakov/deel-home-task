8256441135b8d36d7cb020a74b760b31ac92b098
Yury Kosyakov <6625874+ykosyakov@users.noreply.github.com>
2022-07-03 14:39:22 +0300

    Add /balances/deposit/:userId endpoint

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const { sequelize } = require('./model');
 const setupContractRoutes = require('./routes/contracts');
 const setupJobsRoutes = require('./routes/jobs');
 const setupAdminRoutes = require('./routes/admin');
+const setupBalancesRoutes = require('./routes/balances');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,5 +14,6 @@ app.set('models', sequelize.models);
 setupContractRoutes(app);
 setupJobsRoutes(app);
 setupAdminRoutes(app);
+setupBalancesRoutes(app);
 
 module.exports = app;
---
+++ src/routes/balances.js
@@ -0,0 +1,48 @@
+const { param, body } = require('express-validator');
+const { getProfile } = require('../middleware/getProfile');
+const { validate } = require('../middleware/validate');
+
+function setupBalancesRoutes(app) {
+	const { Profile } = app.get('models');
+	const sequelize = app.get('sequelize');
+
+	app.post('/balances/deposit/:userId', getProfile, param('userId').isNumeric(), body('amount').isNumeric(), validate, async (req, res) => {
+
+		const { userId } = req.params;
+		const { amount } = req.body;
+
+		try {
+			await sequelize.transaction(async (transaction) => {
+				const totalJobsAmountRaw = await sequelize.query('select sum(price) totalJobsAmount from "Jobs" where ContractId in (select id from Contracts where ClientId = :userId)',
+					{
+						replacements: { userId },
+						type: 'SELECT'
+					});
+
+				const totalJobsAmount = totalJobsAmountRaw[0]?.totalJobsAmount ?? 0;
+
+				if (amount > totalJobsAmount * 0.25) {
+					throw new Error(`Deposit amount couldn't be more than ${totalJobsAmount * 0.25}`);
+				}
+
+				const client = await Profile.findOne({ where: { id: userId } });
+
+				client.balance += amount;
+
+				await client.save({ transaction });;
+			});
+
+		}
+		catch (e) {
+			return res.status(500).json(e.message);
+		}
+
+		return res.status(200).end();
+	});
+
+
+
+}
+
+
+module.exports = setupBalancesRoutes;
