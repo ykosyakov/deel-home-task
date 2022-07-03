ae17206cee41e6cae1f7cb0e1110a45b1a136cb1
Yury Kosyakov <6625874+ykosyakov@users.noreply.github.com>
2022-07-03 13:40:28 +0300

    Add /jobs/:id/pay endpoint

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -2,7 +2,7 @@ const { Op } = require('sequelize');
 const { getProfile } = require('../middleware/getProfile');
 
 function setupJobsRoutes(app) {
-	const { Contract, Job } = app.get('models');
+	const { Contract, Job, Profile } = app.get('models');
 
 	app.get('/jobs/unpaid', getProfile, async (req, res) => {
 		const profileId = req.get('profile_id');
@@ -17,6 +17,50 @@ function setupJobsRoutes(app) {
 		});
 
 		return res.status(200).json(unpaidJobs);
+	});
+	app.post('/jobs/:id/pay', getProfile, async (req, res) => {
+		const sequelize = req.app.get('sequelize');
+		const profileId = req.get('profile_id');
+		const { id: jobId } = req.params;
+
+		try {
+			await sequelize.transaction(async (transaction) => {
+				const job = await Job.findOne({ where: { id: jobId } }, { lock: true, transaction });
+
+				if (job.paid) {
+					throw new Error("Job has been already paid");
+				}
+
+				const client = await Profile.findOne({ where: { id: profileId } }, { transaction, lock: true });
+
+				const contract = await Contract.findOne({
+					where: { id: job.ContractId },
+				}, { transaction });
+
+				const contractor = await Profile.findOne({ where: { id: contract.ContractorId } }, { transaction, lock: true });
+
+				if (client.balance <= job.price) {
+					throw new Error("Insufficient deposit");
+				}
+
+				job.paid = true;
+				job.paymentDate = new Date();
+				contractor.balance += job.price;
+				client.balance -= job.price;
+
+				await client.save({ transaction });
+				await contractor.save({ transaction });
+				await job.save({ transaction });
+
+				return true;
+
+			});
+
+		}
+		catch (e) {
+			return res.status(500).json(e.message);
+		}
+		return res.status(200).end();
 	});
 }
 
